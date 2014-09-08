8dc3c2453044f17e59fae6d72627abc76079ab0a
Miguel Ángel Gutierrez Fernández <magfernandez@users.noreply.github.com>
2014-09-08 16:50:50 +0200

    Visualizator updated with methods for showing demo ElementWidgets

--- Visualizator/Visualizator.cpp
+++ Visualizator/Visualizator.cpp
@@ -1,14 +1,52 @@
 #include "Visualizator.hpp"
 #include "ui_Visualizator.h"
 
-Visualizator::Visualizator(QWidget *parent) :
+Visualizator::Visualizator(ElementList *aElementList, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Visualizator)
 {
     ui->setupUi(this);
+
+    drawElementList( aElementList );
 }
 
 Visualizator::~Visualizator()
 {
+    for (int i=0; i<theElements.size(); i++)
+    {
+        delete theElements.at( i );
+    }
     delete ui;
+}
+
+void Visualizator::drawElementList( ElementList * aList, int startPositionX, int startPositionY )
+{
+    Element * aCurrentElement = aList->getFirstElement();
+    int positionX = 0;
+    int positionY = 0;
+
+
+    while ( aCurrentElement!=NULL )
+    {
+        drawSingleElement( aCurrentElement, positionX, positionY );
+        aCurrentElement = aCurrentElement->getNext();
+        positionX = positionX + 50;
+        positionY = positionY + 50;
+    }
+}
+
+
+
+void Visualizator::drawSingleElement( Element * aElement, int posX, int posY )
+{
+    ElementWidget * aWidget = new ElementWidget( this );
+    theElements.append( aWidget );
+    theWidgetMap[ aElement ] = aWidget;
+    aWidget->setName( aElement->getCompleteName() );
+    if ( aElement->getGender()==Element::FEMALE)
+        aWidget->setFemale( true );
+    else
+        aWidget->setFemale( false );
+    aWidget->setSelected( false );
+    aWidget->move( posX, posY );
 }
--- Visualizator/Visualizator.hpp
+++ Visualizator/Visualizator.hpp
@@ -1,7 +1,10 @@
-#ifndef VISUALIZATOR_HPP
-#define VISUALIZATOR_HPP
+#ifndef __VISUALIZATOR_HPP
+#define __VISUALIZATOR_HPP
 
 #include <QMainWindow>
+#include "Nodes/ElementList.hpp"
+#include "Visualizator/ElementWidget.hpp"
+#include <QPainter>
 
 namespace Ui {
 class Visualizator;
@@ -12,11 +15,33 @@ class Visualizator : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit Visualizator(QWidget *parent = 0);
+    explicit Visualizator( ElementList * aElementList, QWidget *parent = 0);
     ~Visualizator();
 
 private:
-    Ui::Visualizator *ui;
+
+    QList<ElementWidget*> theElements; // ElementWidgets created in the Ui
+    std::map< Element*, ElementWidget*> theWidgetMap; // Relationship between a Element and its widget
+
+    Ui::Visualizator *ui; // User interface
+
+    /**
+     * @brief drawElementList Draws icons on visualizator window starting
+     * from the specified position
+     * @param aList List to be drawn
+     * @param startPositionX initial x position (pixels)
+     * @param startPositionY initial y position (pixels)
+     */
+    void drawElementList( ElementList * aList, int startPositionX = 0, int startPositionY = 0 );
+
+    /**
+     * @brief drawSingleElement Draws a single element and adds it
+     * to the widget list and map
+     * @param aElement Element to be drawn
+     * @param posX x position where the widget will be drawn
+     * @param posY y position where the widget will be drawn
+     */
+    void drawSingleElement( Element * aElement, int posX, int posY );
 };
 
-#endif // VISUALIZATOR_HPP
+#endif // __VISUALIZATOR_HPP
