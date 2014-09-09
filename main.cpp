62e98858c208adae006706f2004ed71c5c8feb75
Miguel Ángel Gutierrez Fernández <magfernandez@users.noreply.github.com>
2014-09-09 23:54:25 +0200

    Updated sample list instances creation

--- main.cpp
+++ main.cpp
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
     runner.run();
 
     ElementList aList;
-    aList.addElement( "John", "Doe", Element::MALE, "Fox", "McCloud", "Jenna", "Jameson", 1954, 11, 7 );
-    aList.addElement( "Jimmy", "Doe", Element::MALE, "John", "Doe", "Anna", "Frank", 1976, 6, 22 );
-    aList.addElement( "Anna", "Doe", Element::FEMALE,"John", "Doe", "Anna", "Frank", 1979, 10, 14 );
-    aList.addElement( "Robert", "Gates", Element::MALE, "Bill", "Gates", "Anna", "Doe", 2004, 3, 27 );
+    aList.addElement( "John", "Doe", Element::MALE, "Fox", "McCloud", "Jenna", "Jameson", 1954, 11, 7, "New York" );
+    aList.addElement( "Jimmy", "Doe", Element::MALE, "John", "Doe", "Anna", "Frank", 1976, 6, 22, "Paris" );
+    aList.addElement( "Anna", "Doe", Element::FEMALE,"John", "Doe", "Anna", "Frank", 1979, 10, 14, "Moscow" );
+    aList.addElement( "Robert", "Gates", Element::MALE, "Bill", "Gates", "Anna", "Doe", 2004, 3, 27, "Pekin" );
     aList.assignRelationships();
     aList.countElements();
 
