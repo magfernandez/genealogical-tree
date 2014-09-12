4601b6bf6512922cfea92183443d93a13d0e03b7
Miguel Ángel Gutierrez Fernández <magfernandez@users.noreply.github.com>
2014-09-13 01:00:28 +0200

    Unit testing for search methods

--- Tests/Nodes/TestElementList.hpp
+++ Tests/Nodes/TestElementList.hpp
@@ -12,6 +12,7 @@ class TestElementList : public CppUnit::TestFixture  {
     CPPUNIT_TEST( testAssignRelationships );
     CPPUNIT_TEST( testFirstElement );
     CPPUNIT_TEST( testDeleteAllElements );
+    CPPUNIT_TEST( testSearches );
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -98,6 +99,54 @@ public:
       CPPUNIT_ASSERT( aListElementInit->countElements()==3);
       aListElementInit->deleteAllElements();
       CPPUNIT_ASSERT( aListElementInit->countElements()==0);
+  }
+
+  void testSearches()
+  {
+      ElementList aNullList;
+      ElementList aList;
+
+      aList.addElement( "John", "Doe", Element::MALE, "Fox", "McCloud", "Jenna", "Jameson", 1954, 11, 7, "New York" );
+      aList.addElement( "Jimmy", "Doe", Element::MALE, "John", "Doe", "Anna", "Frank", 1976, 6, 22, "Paris" );
+      aList.addElement( "Anna", "Doe", Element::FEMALE,"John", "Doe", "Anna", "Frank", 1979, 10, 14, "Moscow" );
+      aList.addElement( "Louise", "Gates", Element::FEMALE, "Bill", "Gates", "Anna", "Doe", 2004, 3, 27, "Pekin" );
+      aList.addElement( "Miguel", "Gutierrez", Element::MALE, "Jose Luis", "Gutierrez", "Maria Asuncion", "Fernandez", 1985, 8, 15, "Ponferrada" );
+      aList.addElement( "Ernie", "Gates", Element::MALE, "Bill", "Gates", "Anna", "Doe", 1987, 12, 12, "Valdepeñas" );
+      aList.addElement( "Ernie", "Roberts", Element::MALE, "Kenny", "Roberts", "Louise", "Gates", 2011, 8, 3, "Murcia" );
+
+      aList.assignRelationships();
+      aList.countElements();
+
+      std::list<Element*> aResultList = aList.searchDescendantsByName( "Jimmy", "Ernie");
+      CPPUNIT_ASSERT( aResultList.size()==2 );
+
+      aResultList = aList.searchByName( "Ernie" );
+      CPPUNIT_ASSERT( aResultList.size()==2 );
+
+      aResultList = aNullList.searchByName( "Ernie" );
+      CPPUNIT_ASSERT( aResultList.size()==0 );
+
+      aResultList = aList.searchByName( "Rodrigo" );
+      CPPUNIT_ASSERT( aResultList.size()==0 );
+
+      aResultList = aList.searchBySurname( "Doe" );
+      CPPUNIT_ASSERT( aResultList.size()==3 );
+
+      aResultList = aNullList.searchBySurname( "Doe" );
+      CPPUNIT_ASSERT( aResultList.size()==0 );
+
+      aResultList = aList.searchByBirthDate( 2004, 3, 27 );
+      CPPUNIT_ASSERT( aResultList.size()==1 );
+
+      aResultList = aNullList.searchByBirthDate( 2004, 3, 27 );
+      CPPUNIT_ASSERT( aResultList.size()==0 );
+
+      aResultList = aList.searchByLocation( "Ponferrada" );
+      CPPUNIT_ASSERT( aResultList.size()==1 );
+
+      aResultList = aNullList.searchByLocation( "Ponferrada" );
+      CPPUNIT_ASSERT( aResultList.size()==0 );
+
   }
 };
 
