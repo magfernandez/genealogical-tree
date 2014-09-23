36105799245dc5dfaf83f0c9cbadeda53fbcb03a
Miguel Ángel Gutierrez Fernández <magfernandez@users.noreply.github.com>
2014-09-23 22:20:44 +0200

    Updated sample list creation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "Nodes/ElementList.hpp"
+#include "Utils/NameGenerator.hpp"
 
 #include "Tests/Nodes/TestElement.hpp"
 #include "Tests/Nodes/TestElementList.hpp"
@@ -26,12 +27,19 @@ int main(int argc, char *argv[])
 
     aList.addElement( "John", "Doe", Element::MALE, "Fox", "McCloud", "Jenna", "Jameson", 1954, 11, 7, "New York" );
     aList.addElement( "Jimmy", "Doe", Element::MALE, "John", "Doe", "Anna", "Frank", 1976, 6, 22, "Paris" );
-    aList.addElement( "Jimmy", "Doe", Element::MALE, "Joaquim", "Doe", "Jessica", "Lard", 1978, 9, 27, "London" );
+    //aList.addElement( "Jimmy", "Doe", Element::MALE, "Joaquim", "Doe", "Jessica", "Lard", 1978, 9, 27, "London" );
+    //aList.addElement( "Peter", "Gates", Element::MALE,"Jenson", "Gates", "Martha", "Hendricks", 1934, 12, 14, "Springfield" );
+    aList.addElement( "Bill", "Gates", Element::MALE,"Peter", "Gates", "Mia", "Henderson", 1954, 7, 7, "Springfield" );
     aList.addElement( "Anna", "Doe", Element::FEMALE,"John", "Doe", "Anna", "Frank", 1979, 10, 14, "Moscow" );
-    aList.addElement( "Louise", "Gates", Element::FEMALE, "Bill", "Gates", "Anna", "Doe", 2004, 3, 27, "Pekin" );
+    aList.addElement( "Louise", "Gates", Element::FEMALE, "Bill", "Gates", "Anna", "Doe", 1984, 3, 27, "Pekin" );
+    aList.addElement( "Martin", "Kingston", Element::MALE, "Francis", "Kingston", "Jessica", "Craig", 1972, 6, 9, "Pekin" );
+    aList.addElement( "Martha", "Kingston", Element::FEMALE, "Martin", "Kingston", "Louise", "Gates", 2003, 2, 3, "Salamanca" );
+    //aList.addElement( "Anne", "Kingston", Element::FEMALE, "Martin", "Kingston", "Louise", "Gates", 2006, 1, 22, "Newcastle" );
+    aList.addElement( "Jose Luis", "Gutierrez", Element::MALE, "Carlos", "Gutierrez", "Francisca", "Vidal", 1944, 3, 14, "Poblado" );
     aList.addElement( "Miguel", "Gutierrez", Element::MALE, "Jose Luis", "Gutierrez", "Maria Asuncion", "Fernandez", 1985, 8, 15, "Ponferrada" );
+    aList.addElement( "Jose", "Gutierrez", Element::MALE, "Jose Luis", "Gutierrez", "Maria Asuncion", "Fernandez", 1982, 6, 6, "Ponferrada" );
     aList.addElement( "Ernie", "Gates", Element::MALE, "Bill", "Gates", "Anna", "Doe", 1987, 12, 12, "Valdepeñas" );
-    aList.addElement( "Ernie", "Roberts", Element::MALE, "Kenny", "Roberts", "Louise", "Gates", 2011, 8, 3, "Murcia" );
+
 
     std::list<Element*> aResultList = aList.searchDescendantsByName( "Jimmy", "Ernie");
 
@@ -41,12 +49,12 @@ int main(int argc, char *argv[])
     }
 
 
-    /*QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
 
     Visualizator w( &aList );
     w.show();
 
-    return a.exec();*/
-    return 0;
+    return a.exec();
+    //return 0;
 }
